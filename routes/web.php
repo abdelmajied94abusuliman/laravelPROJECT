fc0217ac09b1f9fb9b9759e9ee447aa70a7e3431
abdelmajied94abusuliman <115414116+abdelmajied94abusuliman@users.noreply.github.com>
2023-01-25 22:28:37 +0300

    save

---
+++ routes/web.php
@@ -0,0 +1,68 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Admin\MenuController;
+use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Admin\TableController;
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\ReservationController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+// Route::get('/', [AuthenticatedSessionController::class, 'create'])
+//                 ->name('login');
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+Route::get('/dashboard', function () {
+    return view('layouts.admin');
+})->middleware(['auth'])->name('dashboard');
+
+
+Route::middleware(['auth' , 'admin'])->name('admin.')->prefix('admin')->group(function(){
+    Route::get('/' , [AdminController::class , 'index'])->name('index');
+    Route::resource('/category' , CategoryController::class);
+    Route::post('/category/{category}' , [CategoryController::class , 'update'])->name('cat-update');
+    Route::get('/category/{category}' , [CategoryController::class , 'destroy'])->name('cat-destroy');
+    Route::resource('/menu' , MenuController::class);
+    Route::resource('/reservation' , ReservationController::class);
+    Route::resource('/table' , TableController::class);
+    Route::resource('/user' , UserController::class);
+});
+
+
+
+
+
+
+
+require __DIR__.'/auth.php';
+
+
+
